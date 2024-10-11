ed2c515a7dcdbce728eaef05574a3b79a473471a
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2024-10-11 18:34:46 +0200

    added Author and Books

--- src/main/java/be/ipeters/spring6webapp/domain/Author.java
+++ src/main/java/be/ipeters/spring6webapp/domain/Author.java
@@ -1,15 +1,26 @@
 package be.ipeters.spring6webapp.domain;
 
+import lombok.Data;
+
+import java.util.Set;
+
 /* carlpeters created on 11/10/2024 inside the package - be.ipeters.spring6webapp.domain */
+@Data
 public class Author {
     private String firstName;
     private String lastName;
+    private Set<Book> books;
 
     public Author() {
     }
 
-    public Author(String firstName, String lastName) {
+    public Author(Set<Book> books) {
+        this.books = books;
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.books = books;
     }
 }
